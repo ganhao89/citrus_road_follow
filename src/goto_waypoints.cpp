79097f970e2509c821ca5ec6a05b55551aa3162a
Hao Gan <ganhao89@gmail.com>
2017-05-15 12:24:26 -0400

    go to waypoints

--- src/goto_waypoints.cpp
+++ src/goto_waypoints.cpp
@@ -5,10 +5,15 @@
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 //tell the action client that we want to spin a thread by default
-MoveBaseClient ac("move_base", true);
+
 
 void waypointsCallback(const move_base_msgs::MoveBaseGoal::ConstPtr& msg)
 {
+  MoveBaseClient ac("move_base", true);
+   //wait for the action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
   move_base_msgs::MoveBaseGoal goal;
   goal.target_pose = msg->target_pose;
 
@@ -18,24 +23,19 @@ void waypointsCallback(const move_base_msgs::MoveBaseGoal::ConstPtr& msg)
   ac.waitForResult(ros::Duration(1.0));
 
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
+    ROS_INFO("Hooray, the base moved to the waypoint");
   else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    ROS_INFO("The base failed to move to the waypoint for some reason");
 
 }
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "navigation_goals");
   ros::NodeHandle n; 
-   //wait for the action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
-
-  ros::Subscriber goal_sub = n.subscribe("way_points", 1, waypointsCallback);
+  
+  ros::Subscriber goal_sub = n.subscribe("/way_points", 1, waypointsCallback);
   ros::spin();
   return 0;
 }
-
 
 
