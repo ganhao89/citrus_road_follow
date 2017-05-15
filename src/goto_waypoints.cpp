80b9c10fe93a31ca33f072ce78667c4c2cf5a529
Hao Gan <ganhao89@gmail.com>
2017-05-15 19:12:21 -0400

    goto waypoints

--- src/goto_waypoints.cpp
+++ src/goto_waypoints.cpp
@@ -4,36 +4,52 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-//tell the action client that we want to spin a thread by default
+class GoToGoal
+{ 
 
 
-void waypointsCallback(const move_base_msgs::MoveBaseGoal::ConstPtr& msg)
-{
-  MoveBaseClient ac("move_base", true);
-   //wait for the action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
-  move_base_msgs::MoveBaseGoal goal;
-  goal.target_pose = msg->target_pose;
+public:
+    GoToGoal():
+    ac("move_base", true)
+    {
+        while(!ac.waitForServer(ros::Duration(5.0))){
+            ROS_INFO("Waiting for the move_base action server to come up");
+        }
+	goal_sub = n_.subscribe("/way_points", 1, &GoToGoal::waypointsCallback,this);
+    }
 
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
+    ~GoToGoal()
+    {
+    }
+    void waypointsCallback(const move_base_msgs::MoveBaseGoal::ConstPtr& msg)
+    {
+      //MoveBaseClient ac("move_base", true);
+      //while(!ac.waitForServer(ros::Duration(5.0))){
+      //      ROS_INFO("Waiting for the move_base action server to come up");
+      //}
+      goal_.target_pose = msg->target_pose;
 
-  ac.waitForResult(ros::Duration(1.0));
+      ROS_INFO("Sending goal");
+      ac.sendGoal(goal_);
 
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved to the waypoint");
-  else
-    ROS_INFO("The base failed to move to the waypoint for some reason");
+      ac.waitForResult(ros::Duration(10.0));
 
-}
+      if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        ROS_INFO("Hooray, the base moved to the waypoint");
+      else
+        ROS_INFO("The base failed to move to the waypoint for some reason");
+    }
+private:
+    ros::NodeHandle n_;
+    move_base_msgs::MoveBaseGoal goal_;
+    ros::Subscriber goal_sub;
+    MoveBaseClient ac;
+};
+
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "navigation_goals");
-  ros::NodeHandle n; 
-  
-  ros::Subscriber goal_sub = n.subscribe("/way_points", 1, waypointsCallback);
+  GoToGoal goalobj;
   ros::spin();
   return 0;
 }
